fce29108eff47b53a8425a4d4be9ebe198fbad49
Cameron <hameron18@tamu.edu>
2024-04-25 23:33:03 -0500

    Completed insertion function

--- BPlusTree.cpp
+++ BPlusTree.cpp
@@ -37,7 +37,8 @@ class BPlusTree {
         bool insertion(node* ptr, pair<node*, int>* p1, pair<node*, int>* p2);
         bool deletion(int val);
         void printRoot();
-        void printNode(node* no);
+        void printNode(node* _node);
+        void printTree(node* _node);
 };
 
 // Constructor
@@ -83,7 +84,7 @@ bool BPlusTree::insertion(node* ptr, pair<node*, int>* p1, pair<node*, int>* p2)
                     }
                     index++;
                 }
-                cout << "Index for new value = " << index << endl;                
+                // cout << "Index for new value = " << index << endl;                
 
                 // Move keys around to fit new key
                 for (int i = ptr->numKeys; i > index; i--) {
@@ -111,7 +112,7 @@ bool BPlusTree::insertion(node* ptr, pair<node*, int>* p1, pair<node*, int>* p2)
                     }
                     index++;
                 }
-                cout << "Index for new value = " << index << endl;
+                // cout << "Index for new value = " << index << endl;
 
                 // Create new list to hold all values
                 int* list = new int[order+1];
@@ -137,7 +138,7 @@ bool BPlusTree::insertion(node* ptr, pair<node*, int>* p1, pair<node*, int>* p2)
 
                 // Set r = floor[(n+1)/2]
                 int r = (order + 1) / 2; // integer truncation
-                cout << "r = " << r << endl;
+                // cout << "r = " << r << endl;
 
                 // Put values k_r+1 through k_n+1 in new leaf
                 int j = 0;
@@ -241,6 +242,75 @@ bool BPlusTree::insertion(node* ptr, pair<node*, int>* p1, pair<node*, int>* p2)
                     // Else
                         // Return p' = p'' and k' = kr
 
+                // Find where new value should go
+                index = 0;
+                for (int i = 0; i < ptr->numKeys; i++) {
+                    if (ptr->keys[i] > p__->second) {
+                        break;
+                    }
+                    index++;
+                }
+                // cout << "Index for new value = " << index << endl;
+
+                // Create new list to hold values and pointers
+                int* list = new int[order+1];
+                node** plist = new node*[order+2];
+
+                // Rearrange contnet in ptr and (p'', k'') into new list
+                for (int i = 0; i < index; i++) {
+                    list[i] = ptr->keys[i];
+                    plist[i] = ptr->ptrs[i];
+                }
+                list[index] = p__->second;
+                plist[index] = ptr->ptrs[index];
+                plist[index+1] = p__->first;
+                for (int i = index+1; i < order+1; i++) {
+                    list[i] = ptr->keys[i-1];
+                    plist[i+1] = ptr->ptrs[i];
+                }
+
+                // Create new node for p''
+                node* newInternalNode = new node();
+                newInternalNode->leaf = false;
+
+                // Set r = ceil[(n+1)/2]
+                int r = ((order + 1) / 2) + 1; // integer truncation
+                // cout << "r = " << r << endl;
+
+                // Put half of the list into new node
+                int j = 0;
+                for (int i = r; i <= order; i++) {
+                    newInternalNode->keys[j] = list[i];
+                    newInternalNode->numKeys++;
+                    newInternalNode->ptrs[j] = plist[i];
+                    j++;
+                }
+                newInternalNode->ptrs[j] = plist[order+1];
+
+                // Put other half of list in ptr
+                ptr->numKeys = 0;
+                for (int i = 0; i < r-1; i++) {
+                    ptr->keys[i] = list[i];
+                    ptr->numKeys++;
+                    ptr->ptrs[i] = plist[i];
+                }
+                ptr->ptrs[r-1] = plist[r-1];
+
+                // If ptr is the root, create new root w/ children ptr and newInternalNode
+                if (ptr == root) {
+                    node* newRoot = new node();
+                    newRoot->leaf = false;
+                    newRoot->keys[0] = list[r-1];
+                    newRoot->numKeys++;
+                    newRoot->ptrs[0] = ptr;
+                    newRoot->ptrs[1] = newInternalNode;
+                    root = newRoot;
+                } else {
+                    p2->first = newInternalNode;
+                    p2->second = list[r-1];
+                }
+
+                return true;
             }
         }
     }
@@ -261,11 +331,11 @@ void BPlusTree::printRoot()
     cout << "]" << endl;
 }
 
-void BPlusTree::printNode(node* no)
+void BPlusTree::printNode(node* _node)
 {
     // cout << "Printing..." << endl;
-    int* ks = no->keys;
-    int n = no->numKeys;
+    int* ks = _node->keys;
+    int n = _node->numKeys;
 
     cout << "[";
     for (int i = 0; i < n; i++) {
@@ -275,42 +345,113 @@ void BPlusTree::printNode(node* no)
     // cout << "Finished printing." << endl;
 }
 
+void BPlusTree::printTree(node* _node)
+{
+    if (_node != nullptr) {
+        cout << "[";
+        for (int i = 0; i < _node->numKeys; i++) {
+            cout << _node->keys[i] << ", ";
+        }
+        cout << "]" << endl;
+
+        if (_node->leaf == false) {
+            for (int i = 0; i < _node->numKeys+1; i++) {
+                printTree(_node->ptrs[i]);
+            }
+        }
+    }
+}
+
 int main()
 {
     BPlusTree tree;
     
     bool one = tree.insert(17);
-    cout << "Insertion attempt #1: " << one << endl;
+    // cout << "Insertion attempt #1: " << one << endl;
     bool two = tree.insert(11);
-    cout << "Insertion attempt #2: " << two << endl;
+    // cout << "Insertion attempt #2: " << two << endl;
     bool three = tree.insert(23);
-    cout << "Insertion attempt #3: " << three << endl;
+    // cout << "Insertion attempt #3: " << three << endl;
 
-    tree.printRoot();
+    // tree.printRoot();
+    // cout << endl;
 
     bool four = tree.insert(5);
-    cout << "Insertion attempt #4: " << four << endl;
+    // cout << "Insertion attempt #4: " << four << endl;
     bool five = tree.insert(3);
-    cout << "Insertion attempt #5: " << five << endl;
+    // cout << "Insertion attempt #5: " << five << endl;
     bool six = tree.insert(31);
-    cout << "Insertion attempt #6: " << six << endl;
+    // cout << "Insertion attempt #6: " << six << endl;
 
-    cout << "Root leaf status: " << tree.getRoot()->leaf << endl;
-    tree.printRoot();
-    for (int i = 0; i < tree.getRoot()->numKeys+1; i++) {
-        tree.printNode(tree.getRoot()->ptrs[i]);
-    }
+    // cout << "Root leaf status: " << tree.getRoot()->leaf << endl;
+    // tree.printRoot();
+    // for (int i = 0; i < tree.getRoot()->numKeys+1; i++) {
+    //     tree.printNode(tree.getRoot()->ptrs[i]);
+    // }
+    // cout << endl;
 
     bool seven = tree.insert(2);
-    cout << "Insertion attempt #7: " << seven << endl;
+    // cout << "Insertion attempt #7: " << seven << endl;
     bool eight = tree.insert(29);
-    cout << "Insertion attempt #8: " << eight << endl;
+    // cout << "Insertion attempt #8: " << eight << endl;
 
-    tree.printRoot();
-    for (int i = 0; i < tree.getRoot()->numKeys+1; i++) {
-        // cout << "i = " << i << endl;
-        tree.printNode(tree.getRoot()->ptrs[i]);
-    }
+    // tree.printRoot();
+    // for (int i = 0; i < tree.getRoot()->numKeys+1; i++) {
+    //     // cout << "i = " << i << endl;
+    //     tree.printNode(tree.getRoot()->ptrs[i]);
+    // }
+    // cout << endl;
+
+    bool nine = tree.insert(50);
+    // cout << "Insertion attempt #9: " << nine << endl;
+    bool ten = tree.insert(51);
+    // cout << "Insertion attempt #10: " << ten << endl;
+    bool eleven = tree.insert(12);
+    // cout << "Insertion attempt #11: " << eleven << endl;
+    bool twelve = tree.insert(13);
+    // cout << "Insertion attempt #12: " << twelve << endl;
+    bool thirteen = tree.insert(14);
+    // cout << "Insertion attempt #13: " << thirteen << endl;
+    bool fourteen = tree.insert(15);
+    // cout << "Insertion attempt #14: " << fourteen << endl;
+    tree.insert(6);
+    tree.insert(7);
+    tree.insert(8);
+    tree.insert(9);
+    tree.insert(52);
+    tree.insert(53);
+    tree.insert(54);
+    tree.insert(55);
+    tree.insert(56);
+
+    cout << endl;
+    // tree.printRoot();
+    // for (int i = 0; i < tree.getRoot()->numKeys+1; i++) {
+    //     // cout << "i = " << i << endl;
+    //     tree.printNode(tree.getRoot()->ptrs[i]);
+    // }
+    // for (int i = 0; i < tree.getRoot()->ptrs[0]->numKeys+1; i++) {
+    //     // cout << "i = " << i << endl;
+    //     tree.printNode(tree.getRoot()->ptrs[0]->ptrs[i]);
+    // }
+    // for (int i = 0; i < tree.getRoot()->ptrs[1]->numKeys+1; i++) {
+    //     // cout << "i = " << i << endl;
+    //     tree.printNode(tree.getRoot()->ptrs[1]->ptrs[i]);
+    // }
+    // for (int i = 0; i < tree.getRoot()->ptrs[2]->numKeys+1; i++) {
+    //     // cout << "i = " << i << endl;
+    //     tree.printNode(tree.getRoot()->ptrs[2]->ptrs[i]);
+    // }
+    // for (int i = 0; i < tree.getRoot()->ptrs[3]->numKeys+1; i++) {
+    //     // cout << "i = " << i << endl;
+    //     tree.printNode(tree.getRoot()->ptrs[3]->ptrs[i]);
+    // }
+    // cout << endl;
+
+    tree.insert(57);
+
+    tree.printTree(tree.getRoot());
+    cout << endl;
 
     cout << "Program finished." << endl;
     return 0;
