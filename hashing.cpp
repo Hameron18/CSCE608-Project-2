6ec8eb8c593e217f966317c4c57fe3b954f79a50
Cameron <hameron18@tamu.edu>
2024-04-28 14:13:40 -0500

    Added data generation for relation S in hashing file

--- hashing.cpp
+++ hashing.cpp
@@ -6,16 +6,92 @@
 
 using namespace std;
 
+struct block {
+    pair<string, int>* tuples;
+    int numTuples;
+    int id;
+    string relationName;
+    block()
+    {
+        id = -1;
+        numTuples = 0;
+        tuples = new pair<string, int>[8]();
+        relationName = "";
+    }
+};
 
+string generateRandomString(int l)
+{
+    string characters = "ACBDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    string result = "";
+    int randInt;
+
+    for (int i = 0; i < l; i++) {
+        randInt = rand() % characters.length();
+        result += characters[randInt];
+    }
+
+    // cout << "Result = " << result << endl;
+
+    return result;
+}
+
+void generateRelationS(vector<block*>* _disk)
+{
+    block* b;
+    int randInt = 1;
+    int randVal = 10000;
+    int count = 0;
+    int numVals = 5000;
+    int blockSize = 8;
+    
+    while (count < numVals) {
+        // Create new tuple
+        b = new block();
+        b->id = _disk->size();
+        b->relationName = "S";
+
+        for (int i = 0; i < blockSize; i++) {
+            // Create a random key
+            randInt = rand() % 20;
+            randVal += randInt;
+
+            // Create a random string
+            string randString = generateRandomString(5);
+
+            // Input values into tuple
+            b->tuples[i].first = randString;
+            b->tuples[i].second = randVal;
+
+            // Increment count and number of tuples in block
+            b->numTuples++;
+            count++;
+
+            // Check limit
+            if (count == numVals) {
+                break;
+            }
+        }
+
+        // Push new block to the disk;
+        _disk->push_back(b);
+    }
+}
 
 int main()
 {
-    pair<string, int>* t;
+    block* mainMemory = new block[15]();
+    vector<block*>* disk = new vector<block*>();
 
-    t = new pair<string, int>("Test tuple", 1);
+    generateRelationS(disk);
 
-    cout << "The values of t are: ";
-    cout << t->first << " and " << t->second << endl;
+    cout << "Printing blocks..." << endl;
+    for (int i = 0; i < disk->size(); i++) {
+        cout << "Block #" << disk->at(i)->id << " for relation " << disk->at(i)->relationName << ":" << endl;
+        for (int j = 0; j < disk->at(i)->numTuples; j++) {
+            cout << "-- [" << disk->at(i)->tuples[j].first << ", " << disk->at(i)->tuples[j].second << "]" << endl;
+        }
+    }
 
     cout << endl;
     cout << "Program finished." << endl;
